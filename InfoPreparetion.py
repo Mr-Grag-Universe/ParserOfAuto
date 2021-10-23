db4625a295ef62b3ca7ec050d203474cad129880
Stepan <76563428+Mr-Grag-Universe@users.noreply.github.com>
2021-10-23 13:01:44 +0300

    Add files via upload

---
+++ InfoPreparetion.py
@@ -0,0 +1,141 @@
+def info_preparation(all_cars_info):
+    header = []
+    # убираем лишние колонки, перенося из лишних столбцов информацию
+    for i in range(len(all_cars_info)):
+        len_ = len(all_cars_info[i])
+        for j in range(len_):
+            try:
+                if len(all_cars_info[i][j]) > 2:
+                    if all_cars_info[i][j][0] == 'Комплектация':
+                        pass
+                    elif all_cars_info[i][j][0] == 'Двигатель':
+                        pass
+                    elif all_cars_info[i][j][0] == 'Налог':
+                        all_cars_info[i][j][1] = all_cars_info[i][j][3]
+                    else:
+                        print(all_cars_info[i][j])
+                    all_cars_info[i][j] = [all_cars_info[i][j][0], all_cars_info[i][j][1]]
+                elif len(all_cars_info[i][j]) < 2:
+                    all_cars_info[i][j] = [all_cars_info[i][j][0], '']
+
+                if header.count(all_cars_info[i][j][0]) == 0:
+                    if all_cars_info[i][j][0] == 'Двигатель':
+                        if header.count('Топливо') == 0:
+                            header.append('Объём дв.')
+                            header.append('Лошадиные силы')
+                            header.append('Топливо')
+                    else:
+                        header.append(all_cars_info[i][j][0])
+
+            except IndexError:
+                if not all_cars_info[i]:
+                    continue
+                '''if len(all_cars_info[i]) == 0:
+                    continue
+                if not all_cars_info[i][j]:
+                    all_cars_info = all_cars_info[i][:j:] + all_cars_info[i][j + 1::]
+                    j -= 1
+                    len_ -= 1
+                    print("empty")
+                    # print(all_cars_info[i])
+                    continue'''
+                print("IndexError1")
+
+    for i in range(len(all_cars_info)):
+        for j in range(len(all_cars_info[i])):
+            try:
+                if all_cars_info[i][j][0] == 'Цена':
+                    if all_cars_info[i][j][1] is None:
+                        all_cars_info[i][j][1] = '0'
+                        continue
+
+                    elif all_cars_info[i][j][1] != '0':
+                        price = all_cars_info[i][j][1]
+                        new_price = price.split(" ")
+                        if 'от' in new_price[0]:
+                            new_price = new_price[1::]
+                        if new_price[-1] == "₽":
+                            new_price = new_price[:-1:]
+                        price = ""
+                        for a in new_price:
+                            price += a
+                        all_cars_info[i][j][1] = price
+
+                if all_cars_info[i][j][0] == 'Пробег':
+                    if all_cars_info[i][j][1] is None:
+                        all_cars_info[i][j][1] = '0'
+                        continue
+
+                    elif all_cars_info[i][j][1] != '0':
+                        run = all_cars_info[i][j][1]
+                        new_price = run.split(" ")
+                        run = ""
+                        for a in new_price:
+                            if a.isdigit():
+                                run += a
+                        all_cars_info[i][j][1] = run
+
+                if all_cars_info[i][j][0] == 'Налог':
+                    if all_cars_info[i][j][1] is None:
+                        all_cars_info[i][j][1] = '0'
+                        continue
+
+                    elif all_cars_info[i][j][1] != '0':
+                        tax = all_cars_info[i][j][1]
+                        new_price = tax.split(" ")
+                        tax = ""
+                        for a in new_price:
+                            if a.isdigit():
+                                tax += a
+                        all_cars_info[i][j][1] = tax
+
+                if all_cars_info[i][j][0] == 'Владельцы':
+                    if all_cars_info[i][j][1] is None:
+                        all_cars_info[i][j][1] = '0'
+                        continue
+
+                    elif all_cars_info[i][j][1] != '0':
+                        owners = all_cars_info[i][j][1]
+                        new_price = owners.split(" ")
+                        owners = ""
+                        for a in new_price:
+                            if a.isdigit():
+                                owners += a
+                        all_cars_info[i][j][1] = owners
+
+                if all_cars_info[i][j][0] == 'Двигатель':
+                    if all_cars_info[i][j][1] is None:
+                        #all_cars_info[i][j][1] = '0'
+                        continue
+
+                while "\xa0" in all_cars_info[i][j][0]:
+                    all_cars_info[i][j][0] = all_cars_info[i][j][0].replace(" ", ' ')
+                    break
+
+                while "\xa0" in all_cars_info[i][j][1]:
+                    all_cars_info[i][j][1] = all_cars_info[i][j][1].replace(" ", ' ')
+                    break
+
+            except IndexError:
+                print("IndexError")
+
+    for i in range(len(all_cars_info)):
+        for j in range(len(all_cars_info[i])):
+            try:
+                if all_cars_info[i][j][0] == 'Двигатель':
+                    char = all_cars_info[i][j][1].split(' ')
+                    all_cars_info[i] = all_cars_info[i][:j:] + all_cars_info[i][j+1::]
+                    all_cars_info[i].append(['Объём дв.', ''])
+                    all_cars_info[i].append(['Лошадиные силы', ''])
+                    all_cars_info[i].append(['Топливо', ''])
+                    if not char:
+                        break
+                    all_cars_info[i][-1][1] = char[-1]
+                    all_cars_info[i][-2][1] = char[3]
+                    all_cars_info[i][-3][1] = char[0]
+                    #print(all_cars_info[i])
+                    break
+            except IndexError:
+                print("Error")
+
+    return header, all_cars_info
---
+++ WorkWithCar.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+
+def get_characteristic(url):
+    # открытие объявления
+    r = requests.get(url)
+    r.encoding = 'utf-8'
+
+    # копируем код страницы
+    content_of_html_file = r.text
+    soup = bs(content_of_html_file, 'html.parser')
+
+    list_of_char = []
+    table = []
+
+    price = soup.find("div", "CardHead__price CardHead__topRowRightColumn")
+    if price is not None:
+        price = price.find("span", "OfferPriceCaption__price")
+
+    #print(price)
+    if price is not None:
+        price = price.text
+    table.append(["Цена", price])
+
+    #if it's a dialler offer
+    if soup.find("svg", "IconSvg IconSvg_official-dealer IconSvg_size_48 CardSellerNamePlace__official-dealer-sign") is None:
+        # берём элемент, содержащий характеристики машины
+        container_with_table = soup.find('div', 'CardOfferBody__leftColumn')
+        if container_with_table is None:
+            container_with_table = soup.find("ul")
+        if container_with_table is None:
+            print("Error here")
+            return []
+
+        # разбиваем на части
+        list_of_char = container_with_table.find_all("li")
+
+        list_of_char = list(map(lambda x: x.find_all("span"), list_of_char))
+
+        # генерируем общую табличку свойств и параметров
+        table += [list(map(lambda x: x.text, list_of_char[i])) for i in range(len(list_of_char))]
+    else:
+        container_with_table = soup.find("ul", "CardInfoGrouped__list")
+        list_of_char = container_with_table.find_all("li")
+        for li in list_of_char:
+            container = li.find("div", "CardInfoGroupedRow__cellLabel")
+            tags = container.find_all("div") + container.find_all("a")
+            temp_list = []
+            for tag in tags:
+                temp_list.append(tag.text)
+            table.append(temp_list)
+
+    return table
